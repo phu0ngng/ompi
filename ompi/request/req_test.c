d80d3caaea92b08cecc731e75a8d10944abce9e4
Joseph Schuchart <schuchart@icl.utk.edu>
2021-10-20 21:49:44 -0400

    Remove re-iteration of continuation requests in test_any

--- ompi/request/req_test.c
+++ ompi/request/req_test.c
@@ -175,9 +175,6 @@ int ompi_request_default_test_any(
             /* continuations may elect to not participate in global progress
              * so progress them separately. */
             ompi_continue_progress_request(request);
-            /* requery the request */
-            --i;
-            --rptr;
         }
 #endif // OMPI_HAVE_MPI_EXT_CONTINUE
     }
