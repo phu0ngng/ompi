88164d43b1f0c23d3bc240c3cf3a0dc49d7d9287
Joseph Schuchart <schuchart@icl.utk.edu>
2022-07-01 15:37:47 -0400

    Revert "Atomics: add relaxed load and store"
    This reverts commit 4518dedc313e95639cc8ebd37879b381d0c0c130.

--- opal/include/opal/sys/atomic.h
+++ opal/include/opal/sys/atomic.h
@@ -664,12 +664,6 @@ static inline intptr_t opal_atomic_fetch_sub_ptr(opal_atomic_intptr_t *addr, voi
 
 #endif /* (OPAL_HAVE_ATOMIC_LLSC_32 || OPAL_HAVE_ATOMIC_LLSC_64)*/
 
-/****** Relaxed load and store ********/
-
-#if !defined(OPAL_ATOMIC_HAVE_RELAXED_LOAD_STORE)
-#define OPAL_ATOMIC_RELAXED_LOAD(a) (a)
-#define OPAL_ATOMIC_RELAXED_STORE(a, v) (a) = (v);
-#endif // !OPAL_ATOMIC_HAVE_RELAXED_LOAD_STORE
 END_C_DECLS
 
 #endif /* OPAL_SYS_ATOMIC_H */
